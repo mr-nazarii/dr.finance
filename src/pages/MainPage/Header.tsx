abc864f03c3e8995fc2490bc4b8e2bb8b6aa958e
Nazarii <nazariikubik@gmail.com>
2022-06-01 16:11:54 +0200

    make currency image change on select

--- src/pages/MainPage/Header.tsx
+++ src/pages/MainPage/Header.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useAppSelector } from "../../hooks/hooks";
 import GS from "../../styles/styles";
 import SettingsMenu from "../SettingsMenu";
 
 const Header = (props: any) => {
   // get the setting menu to the different file
   // add an exit button off the settings menu
+  const currency = useAppSelector((state) => state.profile.currency);
+
   return (
     <GS.LogoWrapper jContent="space-between">
       <GS.BlockWallet>
-        <GS.BlockCurrencyImg src={process.env.PUBLIC_URL + "dollarR.png"} />
+        <GS.BlockCurrencyImg src={process.env.PUBLIC_URL + `${currency}.png`} />
         500
       </GS.BlockWallet>
       <GS.BlockWrapperSettings onClick={() => props.setRight(!props.right)}>
