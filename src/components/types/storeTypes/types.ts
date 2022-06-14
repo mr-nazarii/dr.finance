3d277207dadc85f149da81e447c1f3d2595c24e4
Nazarii <nazariikubik@gmail.com>
2022-06-14 10:08:57 +0200

    add new hook useEffectOnce

--- src/components/types/storeTypes/types.ts
+++ src/components/types/storeTypes/types.ts
@@ -10,3 +10,20 @@ export interface ProfileState {
   expenses?: Object;
   total?: Number;
 }
+
+export interface UserFinancesState {
+  bills: number;
+  car: number;
+  clothes: number;
+  comunication: number;
+  eatingPlace: number;
+  entertainment: number;
+  food?: number;
+  gifts?: number;
+  health?: number;
+  house?: number;
+  pets?: number;
+  sports?: number;
+  taxi?: number;
+  transport?: number;
+}
